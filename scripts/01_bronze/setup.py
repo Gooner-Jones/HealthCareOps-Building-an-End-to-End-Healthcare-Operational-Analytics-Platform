0766174235df3a286601407dd7f9d1f9a7791a9c
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2026-08-27 15:17:05 +0200

    Update setup.py

--- scripts/01_bronze/setup.py
+++ scripts/01_bronze/setup.py
@@ -1,8 +1,14 @@
 # ============================================================
 # CELL 1 : Create a text widget for catalog name and retrieve its value
 # ============================================================
+from pyspark.sql.types import (
+    StructType, StructField, StringType, IntegerType,
+    DateType, TimestampType
+) 
+
 dbutils.widgets.text("catalog_name", "hospital_analytics", "Catalog Name")
 catalog_name = dbutils.widgets.get("catalog_name")
+
 
 
 spark.sql(f"CREATE CATALOG IF NOT EXISTS {catalog_name}")
